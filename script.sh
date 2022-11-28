ac632adc92bc8326e5671a3344731a8917a7e9fa
Victor Hugo <victor_hugo19_9@hotmail.com>
2022-11-27 22:32:46 -0300

    atualizacao para codigo final sem ip do load

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Criando as imagens"
+
+docker build -t vihhugo/projeto-backend:1.0 backend/.
+docker build -t vihhugo/projeto-database:1.0 database/.
+
+echo "Realizando o push das imagen"
+
+docker push vihhugo/projeto-backend:1.0
+docker push vihhugo/projeto-database:1.0 
+
+echo "Criando servicos no cluster kubernets"
+
+kubctl apply -f ./servicos.yml
+
+echo "Criando os deployments"
+
+kubctl apply -f ./deployment.yml
